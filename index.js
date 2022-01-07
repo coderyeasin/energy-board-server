43687c65bb396be475c271b59fe2302e85c79b7c
coderyeasin <yeasin3233@gmail.com>
2022-01-08 04:33:06 +0600

    implement server with DB

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Oh! great')
+})
+
+//Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tie3l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+//REST API
+async function server() {
+    try {
+        await client.connect()
+        
+        const database = client.db('statistics')
+        const energyCollection = database.collection('country_status')
+
+        //GET API
+        app.get('/country_status', async (req, res) => {
+            const result = await energyCollection.find({}).toArray()
+            res.send(result)
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+server().catch(console.dir)
+
+
+
+
+app.listen(port, () => {
+  console.log(`Energy Board Listening:${port}`)
+})
